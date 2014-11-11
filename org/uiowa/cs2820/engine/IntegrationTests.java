7472add93aab137d612fe18efab028820611cb41
Nick Evans <nicholas-evans@uiowa.edu>
2014-11-11 12:37:25 -0600

    Begin work on integration tests

---
+++ org/uiowa/cs2820/engine/IntegrationTests.java
@@ -0,0 +1,19 @@
+package org.uiowa.cs2820.engine;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class IntegrationTests {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
