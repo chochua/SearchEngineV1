901626257f716879d421cff851539baa34120c50
jasellers <jacob-sellers@uiowa.edu>
2014-11-12 14:18:42 -0600

    updated diskspace
    updated the file path no longer needing to take in String A as a file
    path

--- org/uiowa/cs2820/engine/DiskSpace.java
+++ org/uiowa/cs2820/engine/DiskSpace.java
@@ -7,24 +7,28 @@ public class DiskSpace {
 	//has ability to throw exceptions if needed
 	
 	//ArrayList<byte[]> database1 = new ArrayList<byte[]>();
-	public static void writeArea(String A, String info, int location)
+	public static void writeArea(String info, int location)
 		throws IOException{
-		//A is the file path
-		RandomAccessFile file = new RandomAccessFile(A,"rw");
+		//Creates the filepath
+		String filepath = "diskspace.txt";
+		File file = new File(filepath);
+		RandomAccessFile file = new RandomAccessFile(file,"rw");
 		//looks for files location
 		file.seek(location);
-		//writes to file with info
+		//writes to file with data given by info
 		file.write(info.getBytes());
 		file.close();
 		
 		
 	}
-	public static byte[] readArea(String A, int i, int size) 
+	public static byte[] readArea(int location, int size) 
 			throws IOException{
-		// creates a new RAF with String A being the file path
-		RandomAccessFile file = new RandomAccessFile(A,"r"); 
-		//locates position of file given by int i
-		file.seek(i); 										
+		String filepath = "diskspace.txt";
+		File file = new File(filepath);
+		// creates a new RAF with String A being the file path		
+		RandomAccessFile file = new RandomAccessFile(file,"r"); 
+		//locates position of file given by int location
+		file.seek(location); 										
 		//creates a new byte array of size of file
 		byte[] data = new byte[size];	
 		//reads data after starting point of given size
