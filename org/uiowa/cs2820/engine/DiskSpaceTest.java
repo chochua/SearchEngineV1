d58332795b576dabd57ec8d04d8d3deda08d55b8
jasellers <jacob-sellers@uiowa.edu>
2014-11-13 13:08:48 -0600

    updated tests
    added test for new file writing

--- org/uiowa/cs2820/engine/DiskSpaceTest.java
+++ org/uiowa/cs2820/engine/DiskSpaceTest.java
@@ -2,15 +2,35 @@ package org.uiowa.cs2820.engine;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.junit.Test;
 
 public class DiskSpaceTest {
+	
+//Tests Write by initializing a new file and testing its existence.
+@Test
+public void Testfirst(){
+	try {
+		DiskSpace.writeArea("Testing", 50);
+	} catch (IOException e) {
+		System.out.println("Error Writing in Test 1");
+		e.printStackTrace();
+		System.out.println(e);
+	}
+	String filepath = "diskspace.txt";
+	File file = new File(filepath);
+	
+	assertEquals(file.exists(),true);
+}
+		
+	
+	
 	//tests the write and read at the same time.
 	//If there is an error in either portion, an error and message is displayed.
 	@Test
-	public void Test() {
+	public void Testsecond() {
 		byte[] test = null;
 		try {
 			DiskSpace.writeArea("abcdef", 0);
@@ -27,16 +47,10 @@ public class DiskSpaceTest {
 			e.printStackTrace();
 			System.out.println(e);
 		}
-		System.out.println(test);
-		System.out.println("Reached assert");
-		assertEquals(test,"abcdef");
 		
+		System.out.println("Reached assert");
+		assertEquals(test.length,6);
 	}
-
-
-
-	
-
-
-
 }
+
+
