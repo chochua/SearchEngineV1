a370138949f0975d2bbdaf8a477be0384ce4a1c3
reniat <reniat@users.noreply.github.com>
2014-11-14 20:42:59 -0600

    Update DiskSpace.java

--- org/uiowa/cs2820/engine/DiskSpace.java
+++ org/uiowa/cs2820/engine/DiskSpace.java
@@ -1,11 +1,12 @@
-//Jake Sellers 00595842
 package org.uiowa.cs2820.engine;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.RandomAccessFile;
 import java.io.IOException;
 public class DiskSpace {
 	// DiskSpace using Random Access File
 	//has ability to throw exceptions if needed
+	
 	
 	//ArrayList<byte[]> database1 = new ArrayList<byte[]>();
 	public static void writeArea(byte[] info, int location)
@@ -13,44 +14,64 @@ public class DiskSpace {
 		//Creates the file path
 		String filepath = "diskspace.txt";
 		File file = new File(filepath);
-		
-	
 			// first time, file doesn't exist
 			if (!file.exists()) {
 				file.createNewFile();
 				System.out.println("file created");
 			}
-		
-		
-		
 		RandomAccessFile RAfile = new RandomAccessFile(file,"rw");
 		//looks for files location
 		RAfile.seek(location);
-		//writes to file with data given by info
+		int N = info.length;
+		// suppose N is 825
+		// next encode N into 2 bytes
+		byte [] E = new byte[2];
+		E[0] = (byte) (N / 256);
+		E[1] = (byte) N;
+		RAfile.write(E);
 		RAfile.write(info);
-		RAfile.close();
-
-		
-		
+		RAfile.close();	
 	}
-	public static byte[] readArea(int location, int size) 
+	public static byte[] readArea(int location) 
 			throws IOException{
 		String filepath = "diskspace.txt";
 		File file = new File(filepath);
 		// creates a new RAF with String A being the file path		
 		RandomAccessFile RAfile = new RandomAccessFile(file,"r"); 
-		//locates position of file given by int location
-		RAfile.seek(location); 										
-		//creates a new byte array of size of file
-		byte[] data = new byte[size];	
-		//reads data after starting point of given size
-		RAfile.read(data);
+		
+		RAfile.seek(location);
+		byte [] E = new byte[2];
+		RAfile.read(E);
+		int N = (int)E[0];
+		if (N < 0) N += 256; // Java has no unsigned
+		N = N * 256;
+		int M = (int)E[1];
+		if (M < 0) M += 256; // stupid Java
+		N = N + M;
+		byte [] Z = new byte[N];
+		RAfile.read(Z);
 		RAfile.close();
 
-		//returns searched for data
-		return data;
+		return Z;
 		
 	}
-	}
-
 	
+	public static int Size(){
+		String filepath = "diskspace.txt";
+		File file = new File(filepath);
+		RandomAccessFile RAfile;
+		try {
+			RAfile = new RandomAccessFile(file,"r");
+			int size = (int)RAfile.length();
+			RAfile.close();
+			return size;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return -1;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return -1;
+		} 
+		
+	}
+}
